f3a13e97b561f809799181f289736d077161cefb
Joshua Mark <joshuajmark@gmail.com>
2017-10-14 15:19:36 -0700

    Icon avatar helper working

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,12 +1,12 @@
 module IconHelper
   def avatar(user, size=:sm)
-    circular_icon image_tag(user.avatar_url(size)) + user.full_name.initials, style: 'tiny', class: 'avatar', alt: user.full_name
+    circular_icon image_tag(user.avatar_url(size), class: 'rounded-circle') + user.full_name.initials, style: size.to_s, class: 'avatar', alt: user.full_name
   end
 
   def circular_icon(content, options={})
     style = options.delete(:style) || 'medium'
     content_tag :span,
       content,
-      options.merge!(class: ['rounded-circle', style, options[:class]].compact.join(' '))
+      options.merge!(class: ['circular-icon', style, options[:class]].compact.join(' '))
   end
 end
