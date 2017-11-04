f590096499f11bdb3dfc8774ab90ccdfcc4d19d2
Joshua Mark <joshuajmark@gmail.com>
2017-11-04 13:06:15 -0700

    Old renaming fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "limestun_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "limestone_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
