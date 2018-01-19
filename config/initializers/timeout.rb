cac19b4ab44fddba9b32dd2e27a3ef0c7ff66804
Joshua Flark <joshuajmark@gmail.com>
2018-01-19 13:51:17 -0800

    Disable timeout for development. Because pry.

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,7 @@
-Rack::Timeout.timeout = ENV.fetch('REQUEST_TIMEOUT') { 5 }.to_i
-Rack::Timeout::Loger.level = Logger::DEBUG
+if Rails.env.development?
+  Rack::Timeout.timeout = false # Disable
+else
+  Rack::Timeout.timeout = ENV.fetch('REQUEST_TIMEOUT') { 5 }.to_i
+end
+
+Rack::Timeout::Logger.level = Logger::DEBUG
