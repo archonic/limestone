d75ae9bb76b8d44765fa2936e880b657716508ae
Joshua Flark <joshuajmark@gmail.com>
2018-01-31 11:09:10 -0800

    create_admin_service test

---
+++ spec/services/create_admin_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'stripe_mock'
+
+RSpec.describe CreateAdminService, type: :service do
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
+  describe '#call' do
+    before { create(:plan) }
+
+    it 'creates the admin' do
+      expect(User.count).to eq 0
+      CreateAdminService.call
+      expect(User.count).to eq 1
+      expect(User.first.admin?).to be true
+    end
+  end
+end
