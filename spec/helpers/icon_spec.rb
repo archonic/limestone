c258989185bab0c4688271e67fbc9a2f37eed148
Joshua Flark <joshuajmark@gmail.com>
2018-02-01 16:29:24 -0800

    Icon tests

---
+++ spec/helpers/icon_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe IconHelper do
+  let(:user) { create(:user) }
+
+  describe "avatar" do
+    it "defaults to small" do
+      expect(avatar(user)).to match ";s=#{IconHelper::AVATAR_SIZES[:sm]}"
+    end
+
+    it "returns avatar" do
+      expect(avatar(user)).to match "circular-icon sm avatar-text"
+    end
+
+    it "has text backup" do
+      expect(avatar(user)).to match user.full_name.initials
+    end
+  end
+
+  describe 'icon' do
+    it 'defaults to small' do
+      expect(icon(:beer)).to match "font-size: #{IconHelper::AVATAR_SIZES[:sm]}px"
+    end
+
+    it 'returns a font awesome icon' do
+      expect(icon(:beer)).to match "fa fa-beer"
+    end
+  end
+end
