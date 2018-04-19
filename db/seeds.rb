3ad2661fcb2bfcc9a0cc10aab6d3317730168617
Joshua Flark <joshuajmark@gmail.com>
2018-04-19 09:22:57 -0700

    Can't use frozen string literal in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
