d6c1642794a08f23cc75f0520ddbb5f4236be1bc
Joshua Flark <joshuajmark@gmail.com>
2018-06-16 12:05:48 -0700

    Update setting timeout to new ENV method

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
+# Set timeouts via ENV vars
+# service_timeout:   15     # RACK_TIMEOUT_SERVICE_TIMEOUT
+# wait_timeout:      30     # RACK_TIMEOUT_WAIT_TIMEOUT
+# wait_overtime:     60     # RACK_TIMEOUT_WAIT_OVERTIME
+# service_past_wait: false  # RACK_TIMEOUT_SERVICE_PAST_WAIT
 
-Rack::Timeout.timeout = if Rails.env.development?
-                          false
-                        else
-                          ENV.fetch('REQUEST_TIMEOUT') { 5 }.to_i
-                        end
 Rack::Timeout::Logger.level = Logger::DEBUG
