9471b4fef22cb7746147d2b38c89729822149c0a
Joshua Flark <joshuajmark@gmail.com>
2020-03-14 16:48:06 -0700

    Bring plan creation up to date with stripe 2019-02-11 and make it safer

--- app/services/create_plan_service.rb
+++ app/services/create_plan_service.rb
@@ -7,18 +7,21 @@ class CreatePlanService
 
   def call
     stripe_plan = nil
+    stripe_plan_attrs = {
+      product: {
+        name: @plan_model.name
+      },
+      id: @plan_model.name.downcase.tr(' ', '-'),
+      amount: @plan_model.amount,
+      interval: @plan_model.interval,
+      currency: @plan_model.currency,
+      trial_period_days: TRIAL_PERIOD_DAYS
+    }
+    StripeLogger.info "Creating Plan/Product: #{stripe_plan_attrs}"
     begin
-      stripe_plan = Stripe::Plan.create(
-        id: [@plan_model.name.downcase, SecureRandom.uuid].join("_"),
-        name: @plan_model.name,
-        amount: @plan_model.amount,
-        interval: @plan_model.interval,
-        currency: @plan_model.currency,
-        trial_period_days: TRIAL_PERIOD_DAYS
-      )
+      stripe_plan = Stripe::Plan.create stripe_plan_attrs
     rescue Stripe::InvalidRequestError => e
       StripeLogger.error "Error creating plan #{@plan_model.name}: #{e}"
-      raise ActiveRecord::RecordInvalid
     end
 
     # Don't hit the DB here as this is performed in before_create
