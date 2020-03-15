832c486d3850c42294ae07f7b96e76a7f1fbad1d
Joshua Flark <joshuajmark@gmail.com>
2020-03-15 12:28:34 -0700

    Fix create plan spec

--- spec/services/create_plan_service_spec.rb
+++ spec/services/create_plan_service_spec.rb
@@ -1,27 +1,23 @@
 # frozen_string_literal: true
 
-class CreatePlanService
-  def initialize(plan_model)
-    @plan_model = plan_model
+require "rails_helper"
+require "stripe_mock"
+
+RSpec.describe CreatePlanService, type: :service do
+  let(:plan) { create(:plan) }
+  before do
+    allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+    StripeMock.start
+  end
+  after do
+    allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("test"))
+    StripeMock.stop
   end
 
-  def call
-    stripe_plan = nil
-    begin
-      stripe_plan = Stripe::Plan.create(
-        id: [@plan_model.name.downcase, SecureRandom.uuid].join('_'),
-        name: @plan_model.name,
-        amount: @plan_model.amount,
-        interval: @plan_model.interval,
-        currency: @plan_model.currency,
-        trial_period_days: TRIAL_PERIOD_DAYS
-      )
-    rescue Stripe::InvalidRequestError => e
-      StripeLogger.error "Error creating plan #{@plan_model.name}: #{e}"
-      raise ActiveRecord::RecordInvalid
+  describe "#call" do
+    it "creates the plan" do
+      expect(Stripe::Plan).to receive(:create).once
+      CreatePlanService.new(plan)
     end
-
-    # Don't hit the DB here as this is performed in before_create
-    @plan_model.stripe_id = stripe_plan.id if stripe_plan.present?
   end
 end
