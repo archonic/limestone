5d1ffc5a86bdb50f97c7a22a6969a2b4282183a8
Joshua Flark <joshuajmark@gmail.com>
2020-06-29 18:47:59 -0700

    rubocop

--- db/migrate/20200629212235_users_rename_product_to_plan.rb
+++ db/migrate/20200629212235_users_rename_product_to_plan.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersRenameProductToPlan < ActiveRecord::Migration[6.0]
   def change
     rename_column :users, :product_id, :plan_id
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_06_29_212235) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,5 +115,4 @@ ActiveRecord::Schema.define(version: 2020_06_29_212235) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
