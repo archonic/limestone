dc247d750ff3d5422a0a86d1b0cc97d658410d27
Joshua Flark <joshuajmark@gmail.com>
2020-07-02 16:16:43 -0700

    Important notes

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -10,6 +10,12 @@ Pay.setup do |config|
   config.application_name = "Limestone App"
   config.support_email = "support@example.com"
 
+  # NOTE Stripe can send styled and branded emails on your behalf
+  # https://dashboard.stripe.com/settings/billing/automatic
+  # If you turn on emails for upcoming renewal, failed payments and invoicing,
+  # make sure you're not doubling up emails for the same event here.
+  # See Pay's emails here:
+  # https://github.com/pay-rails/pay/blob/master/app/mailers/pay/user_mailer.rb
   config.send_emails = true
 
   config.automount_routes = true
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,10 @@
 sidekiq_config = { url: ENV["ACTIVE_JOB_URL"] }
 
 Sidekiq.configure_server do |config|
+  # NOTE docker-compose redis uses an AOF file which grows indefinitely
+  # This increases startup time until the volume is wiped.
+  # Delete the volume if this becomes unreasonably long (provided the data is not needed).
+  sleep 10
   config.redis = sidekiq_config
 end
 
