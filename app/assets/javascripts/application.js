92cdc4c1f3936db0e0aca43e99f2c66741429f6e
Joshua Flark <joshuajmark@gmail.com>
2021-02-23 17:43:10 -0800

    Get rid of asset pipeline and move webpack assets

--- app/assets/javascripts/application.js
+++
@@ -1,4 +0,0 @@
-// Requires do not get processed here. Load everything through webpack.
-// https://github.com/rails/webpacker/issues/21
-
-console.log('Hello World from Sprockets (app/assets/javascripts/application.js)')
