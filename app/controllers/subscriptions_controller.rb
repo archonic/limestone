fe76a9a6054c12e165f7d9210742b1184cab80c1
Joshua Flark <joshuajmark@gmail.com>
2021-03-09 09:49:34 -0800

    Light refactor

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -19,11 +19,13 @@ class SubscriptionsController < ApplicationController
   end
 
   # PATCH /subscriptions
+  # NOTE This handles both card updates and plan swaps
   def update
-    current_user.processor = "stripe"
     success = if params[:plan_id].present?
-      SubscriptionService.new(current_user, params[:plan_id]).swap_plan!
+      plan = Plan.active.find(params[:plan_id])
+      SubscriptionService.new(current_user, plan).swap_plan!
     elsif params[:payment_method_id].present?
+      current_user.processor = "stripe"
       current_user.update_card(params[:payment_method_id])
     end
     if success
